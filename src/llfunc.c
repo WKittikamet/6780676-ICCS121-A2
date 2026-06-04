4a8277eb86602282f25a1c4feceebff138b5aca5
Waranyu Kittikamet (WKittikamet) <waranyu.kit@student.mahidol.edu>
2026-06-04 23:58:30 +0700

    intialized src directory and key .c files

---
+++ src/llfunc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <linkedlist.h>
+
+// Add a node to the list with information at position pos
+void add(Node *list, long value, char *words, int pos){
+
+};
+// Delete node at position pos
+void dlt(Node *list, int pos){
+
+};
+
+// Clears all memory allocations
+void clear_memory(Node *list){
+
+};
