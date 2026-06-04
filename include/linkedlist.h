24eb46b820df35f8f49a58f31b85e418c1f40bfe
Waranyu Kittikamet (WKittikamet) <waranyu.kit@student.mahidol.edu>
2026-06-04 23:54:36 +0700

    added Node struct and functions

--- include/linkedlist.h
+++ include/linkedlist.h
@@ -1,8 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+// Defining the LinkedList Node structure;
 typedef strcut Node {
 	struct Node *head;
 	long amnt;
+	char *desc;
 	struct Node *tail;
 } Node;
+
+// Functions used for LinkedList Nodes
+void add(Node **list, long value, char *str, int pos);
+void dlt(Node **list, int pos);
+void clearmem(Node **list);
