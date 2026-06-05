9d3df1afb16d72d2a09ae585ef0789bdd9f88541
Waranyu Kittikamet (WKittikamet) <waranyu.kit@student.mahidol.edu>
2026-06-05 15:07:39 +0700

    added unfinished code to llfunc.c and main.c

--- src/llfunc.c
+++ src/llfunc.c
@@ -1,17 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <linkedlist.h>
+#include "../include/linkedlist.h"
 
-// Add a node to the list with information at position pos
-void add(Node *list, long value, char *words, int pos){
-
+// Add a node to the circular, dynamic linked list with information at position pos
+void add(Node *list, long value, char *words, int pos, int length){
+	Node *current = *list;
+	Node *n = (Node*)malloc(sizeof(Node));
+	if (n == NULL){
+		printf("Node memory allocation failed");
+		return;
+	}
+	n->amnt = value;
+        n->desc = *words;
+	if (*list == NULL && pos = 0){
+		n->head = NULL;
+		n->tail = NULL;
+		current = n;
+		return;
+	}
+	if (pos > length){
+                printf("Location out of bounds");
+                return;
+        }
+	else if (pos > length/2){
+		current = current->head;
+		for (int i = 0; i < length-pos; i++){
+			current = current->head;
+		}
+	}
+	else {
+		for (int i = 0; i < pos; i++){
+			current = current->tail;
+		}
+	}
+        n->head = current->head;
+        n->tail = current;
+        current->head->tail = n;
+        current->head = n;
+        current=n;
+	return;
 };
+
 // Delete node at position pos
 void dlt(Node *list, int pos){
-
+	Node *current = list;
+	if (pos >= length){
+                printf("Location out of bounds");
+                return;
+        }
+        else if (pos > length/2){
+                current = current->head;
+                for (int i = 0; i < length-pos; i++){
+                        current = current->head;
+                }
+        }
+        else {
+                for (int i = 0; i < pos; i++){
+                        current = current->tail;
+                }
+        }
+	current->head->tail = current->tail;
+	current->tail->head = current->head;
+	free(current);
+	return;
 };
 
 // Clears all memory allocations
 void clear_memory(Node *list){
-
+	Node *current = *list;
+	Node *nextNode;
+	// Looping through every Node and free memory from each one.
+	while (current != NULL){
+		nextNode = current->tail;
+		free(current);
+		current = nextNode;
+	}
+	*list = NULL;
 };
--- src/main.c
+++ src/main.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <linkedlist.h>
+#include <ctype.h>
+#include <string.h>
+#include "../include/linkedlist.h"
 
-int main() {
-
+int main(){
+	char conf[10];
+	Node *list = NULL;
+	
+	printf("**Starting the Program (Loading Previous Transactions)**\n");
+	printf("\n**Original Log File (logs/transaction_log.txt) Before Running the Program:**\n");
+	printf("```\n# Format: TYPE|DESCRIPTION|AMOUNT\n# TYPE: INC (Income) or EXP (Expense)\n# AMOUNT: Positive decimal number\n");
+	printf("\nINC|Freelance Project|250.00\nEXP|Phone Bill|-45.00\nEXP|Electricity Bill|-100.00\n```\n");
+	printf("\n**Program Startup Output:**\n");
+	printf("\n```\nWelcome to your Personal Finance Tracker!\n");
+	while(1) {
+        	printf("Would you like to resume your previous session? (y/n): ");
+		if (fgets(conf, sizeof(conf), stdin) != NULL) {
+			conf[strcspn(conf, "\n")] = '\0';
+			if (strcmp(conf, "y") == 0 || strcmp(conf, "n") == 0) {
+				break;
+			}
+			else {
+				printf("\nConfirmation not recognized\n");
+			}
+		}
+	}
+	if (strcmp(conf, "y") == 0){
+		printf("Resuming from last session...\n");
+		FILE *log = fopen("../logs/transaction_log", "r");
+		if (log != NULL){
+			
+		}
+		else {
+			printf("No previous transactions found. Continuing with new transaction.\n")
+		}
+	}
+	printf("You chose: %s\n", conf);
+	return 0;
 }
