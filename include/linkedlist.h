48e45256fd655d3f81bca09fed3797d7a26cc135
Waranyu Kittikamet (WKittikamet) <waranyu.kit@student.mahidol.edu>
2026-06-10 23:04:23 +0700

    updated Node pointer parameters in the functions

--- include/linkedlist.h
+++ include/linkedlist.h
@@ -6,15 +6,15 @@ typedef struct Node {
 	struct Node *head;
 	long amnt;
 	char *desc;
-	char *status
+	char *status;
 	struct Node *tail;
 } Node;
 
 // Functions used for LinkedList Nodes
-void addlast(Node *list, long value, char *words, int *length, long *total);
-void add(Node *list, long value, char *words, int pos, int *length, long *total);
-long dlt(Node *list, int pos, int *length, long *total);
-void clear_memory(Node *list);
+void addlast(Node **list, long value, char *words, int *length, long *total);
+void add(Node **list, long value, char *words, int pos, int *length, long *total);
+long dlt(Node **list, int pos, int *length, long *total);
+void clear_memory(Node **list);
 void print(Node *list, int *length);
 
 #endif
